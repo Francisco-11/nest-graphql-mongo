845075607c52a4819b5a71a739a2e1c3c6186557
Francisco <francisco.vera@rindegastos.com>
2020-07-27 10:27:04 -0400

    challenge: get all students

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson/lesson.entity';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
+
 
 @Module({
   imports: [
@@ -13,12 +16,13 @@ import { Lesson } from './lesson/lesson.entity';
       url: 'mongodb://localhost/school',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Lesson]
+      entities: [Lesson, Student]
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true
     }),
-    LessonModule
+    LessonModule,
+    StudentModule
   ],
   controllers: [AppController],
   providers: [AppService],
